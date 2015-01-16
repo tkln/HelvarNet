4d1005fa92a1305f1ad0a57e181aab72028a7261
Aapo Vienamo <aapo.vienamo@iki.fi>
2015-01-16 20:45:28 +0200

    Add some encapsulation
    A separate class for the HelvarNet socket and LedUnit

--- helvar.py
+++ helvar.py
@@ -1,16 +1,33 @@
 #!/usr/bin/python3
 import socket
 import time
-HELVARNET_PORT = 50000
-HELVARNET_IP = '10.254.1.2'
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HELVARNET_IP, HELVARNET_PORT))
-time.sleep(1)
-#s.send('>V:2,C:11,G:100,B:2,S:1,F:200#'.encode('ASCII')) #toimii
+import sys
+class HelvarNet:
+    DIRECT_LEVEL = '>V:1,C:14,L:{0},F:{1},@{2}#'
+    def __init__(self, ip, port):
+        self.__s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__s.connect((ip, port))
+    def set_direct_level(self, address, level, fade_time = 0):
+        self.__s.send(self.DIRECT_LEVEL.format(level, fade_time, address).encode())
+
+class LedUnit:
+    def __init__(self, helvarNet, address):
+        self.__net = helvarNet
+        self.__addr = address
+    def set(self, level, fade_time = 0):
+        self.__net.set_direct_level(self.__addr, level, fade_time)
+helvarNet = HelvarNet('10.254.1.2', 50000)        
+leds = [LedUnit(helvarNet, '1.2.1.1'),
+        LedUnit(helvarNet, '1.2.1.2'),
+        LedUnit(helvarNet, '1.2.1.3'),
+        LedUnit(helvarNet, '1.2.1.4'),
+        LedUnit(helvarNet, '1.2.1.5')]
+i = 0
 while (1):
-    s.send('>V:1,C:14,L:0,F:30,@1.2.1.1#'.encode('ASCII')) #toimii
-    time.sleep(0.5)
-    s.send('>V:1,C:14,L:10,F:30,@1.2.1.1#'.encode('ASCII')) #toimii
-    time.sleep(0.5)
-#data = s.recv(128).decode()
-#print(data)
+    leds[i % 5].set(0, 10)
+    time.sleep(0.3)
+    leds[i % 5].set(10, 10)
+    time.sleep(0.3)
+    i += 1
+
+#helvarNet.blink()
